b27ad93a662e989a6dd0c7aa286e899df3f501c2
alexisabels <isabelsanchezalex@gmail.com>
2024-12-17 23:47:22 +0100

    Movie page remake

--- src/pages/Movies/MovieDetails/MovieDetails.jsx
+++ src/pages/Movies/MovieDetails/MovieDetails.jsx
@@ -1,12 +1,12 @@
-// MovieDetails.js
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import axios from "axios";
-import { doc, setDoc, getDoc } from "firebase/firestore";
-import { auth, db } from "../../../../firebaseConfig";
-
-const API_KEY = "ccac83f3684a803c1d8b44b06750a4a7";
-const BASE_URL = "https://api.themoviedb.org/3";
+import { auth } from "../../../../firebaseConfig";
+import { fetchMovieDetails } from "../../../services/tmdbService";
+import {
+  checkIfMovieWatched,
+  markMovieAsUnWatched,
+  markMovieAsWatched,
+} from "../../../services/movieFirebase";
 
 const MovieDetails = () => {
   const { id } = useParams();
@@ -14,83 +14,96 @@ const MovieDetails = () => {
   const [isWatched, setIsWatched] = useState(false);
 
   useEffect(() => {
-    const fetchMovie = async () => {
+    const fetchData = async () => {
       try {
-        const response = await axios.get(`${BASE_URL}/movie/${id}`, {
-          params: {
-            api_key: API_KEY,
-            language: "es-ES", // Puedes cambiar el idioma si lo prefieres
-          },
-        });
-        setMovie(response.data);
-        console.log("Movie data:", response.data);
-      } catch (error) {
-        console.error("Error al obtener detalles de la película:", error);
-      }
-    };
-
-    const checkIfWatched = async () => {
-      try {
+        const movieData = await fetchMovieDetails(id);
+        setMovie(movieData);
+        console.log(movieData);
         const user = auth.currentUser;
         if (user) {
-          const docRef = doc(db, "watchedMovies", `${user.uid}_${id}`);
-          const docSnap = await getDoc(docRef);
-          setIsWatched(docSnap.exists());
-          console.log("Is movie watched:", docSnap.exists());
+          const watched = await checkIfMovieWatched(user.uid, id);
+          setIsWatched(watched);
         }
       } catch (error) {
-        console.error(
-          "Error al verificar si la película está marcada como vista:",
-          error
-        );
+        console.error("Error al cargar los datos:", error);
       }
     };
 
-    fetchMovie();
-    checkIfWatched();
+    fetchData();
   }, [id]);
 
   const handleMarkAsWatched = async () => {
     const user = auth.currentUser;
-    if (user) {
+    if (user && movie) {
       try {
-        const docRef = doc(db, "watchedMovies", `${user.uid}_${id}`);
-        await setDoc(docRef, {
-          userId: user.uid,
-          movieId: id,
+        await markMovieAsWatched(user.uid, id, {
           title: movie.title,
           poster_path: movie.poster_path,
-          watchedAt: new Date(),
         });
         setIsWatched(true);
-        console.log("Película marcada como vista");
       } catch (error) {
         console.error("Error al marcar la película como vista:", error);
       }
     }
   };
 
-  if (!movie) return <div>Cargando....</div>;
+  const handleMarkAsUnWatched = async () => {
+    const user = auth.currentUser;
+    if (user && movie) {
+      try {
+        await markMovieAsUnWatched(user.uid, id);
+        setIsWatched(false);
+      } catch (error) {
+        console.error("Error al marcar la película como no vista:", error);
+      }
+    }
+  };
+
+  if (!movie) return <div>Cargando...</div>;
 
   return (
-    <div>
-      <h2>{movie.title}</h2>
-      <p>{movie.overview}</p>
+    <div className="relative mt-7 flex flex-col lg:flex-row items-center lg:items-start lg:justify-evenly gap-8 p-4">
+    {/* Contenido principal */}
+    {/* Imagen del póster */}
+    <div className="relative flex-shrink-0 max-w-xs lg:max-w-md">
       <img
+        className="w-full rounded-lg shadow-xl"
         src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
         alt={movie.title}
       />
-      <button
-        onClick={handleMarkAsWatched}
-        className={`mt-4 px-4 py-2 rounded ${
-          isWatched ? "bg-gray-500" : "bg-blue-500 hover:bg-blue-600"
-        } text-white transition duration-300`}
-        disabled={isWatched}
-      >
-        {isWatched ? "Ya vista" : "Marcar como vista"}
-      </button>
     </div>
-  );
+    <div className="relative  lg:max-w-lg flex flex-col gap-6 bg-slate-100 bg-opacity-90 rounded-lg shadow-xl p-6">
+      <h2 className="text-4xl text-slate-900 font-extrabold tracking-tight md:text-5xl lg:text-4xl mb-4">
+        {movie.title}
+      </h2>
+      <p className="text-lg text-gray-700 mb-4">{movie.overview}</p>
+  
+      {/* Géneros en chips */}
+      <div className="flex gap-3 flex-wrap mb-4">
+        {movie.genres.map((genre) => (
+          <div key={genre.id} className="rounded-md bg-slate-800 py-1 px-4 border border-transparent text-sm text-white transition-all shadow-sm">
+            {genre.name}
+          </div>
+        ))}
+      </div>
+  
+      <p className="text-gray-700 mb-4">País: {movie.origin_country}</p>
+  {auth.currentUser ?  <button
+        onClick={isWatched ? handleMarkAsUnWatched : handleMarkAsWatched}
+        className={`mt-4 px-6 py-3 rounded-md ${isWatched ? "bg-gray-500 hover:bg-gray-600" : "bg-blue-500 hover:bg-blue-600"} text-white text-lg transition duration-300`}
+      >
+        {isWatched ? "Vista" : "Marcar como vista"}
+      </button> : <button
+        onClick={isWatched ? handleMarkAsUnWatched : handleMarkAsWatched}
+        className={`mt-4 px-6 py-3 rounded-md bg-gray-500 hover:bg-gray-600 text-white text-lg transition duration-300`}
+      >
+        Inicia sesión para marcarla como vista
+      </button>}
+     
+    </div>
+  </div>
+  );  
+  
 };
 
 export default MovieDetails;
